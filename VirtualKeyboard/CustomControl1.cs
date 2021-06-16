32121e33856f3709899db727c380ed0e945df092
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-06-16 23:12:13 +0300

    VirtualKeyboard was changed. Struct InputUnion was added at the path Structs/

--- VirtualKeyboard/CustomControl1.cs
+++ VirtualKeyboard/CustomControl1.cs
@@ -127,15 +127,6 @@ namespace VirtualKeyboard
             }
         }
 
-        [StructLayout(LayoutKind.Explicit)]
-        internal struct InputUnion
-        {
-            [FieldOffset(0)]
-            internal MOUSEINPUT mi;
-            [FieldOffset(0)]
-            internal KEYBDINPUT ki;
-            [FieldOffset(0)]
-            internal HARDWAREINPUT hi;
-        }
+        
     }
 }
---
+++ VirtualKeyboard/Structs/InputUnion.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace VirtualKeyboard.Structs
+{
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct InputUnion
+    {
+        [FieldOffset(0)]
+        internal MOUSEINPUT mi;
+        [FieldOffset(0)]
+        internal KEYBDINPUT ki;
+        [FieldOffset(0)]
+        internal HARDWAREINPUT hi;
+    }
+}
