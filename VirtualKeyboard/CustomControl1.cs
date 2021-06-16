6353ed3e1e31b3c5739a35fbefddfdf927c29e6a
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-06-16 22:51:16 +0300

    VirtualKeyboard was changed. Enum KEYEVENTF was added by the path Enums/

--- VirtualKeyboard/CustomControl1.cs
+++ VirtualKeyboard/CustomControl1.cs
@@ -168,14 +168,7 @@ namespace VirtualKeyboard
             internal UIntPtr dwExtraInfo;
         }
 
-        [Flags]
-        internal enum KEYEVENTF : uint
-        {
-            EXTENDEDKEY = 0x0001,
-            KEYUP = 0x0002,
-            SCANCODE = 0x0008,
-            UNICODE = 0x0004
-        }
+        
 
         internal enum VirtualKeyShort : short
         {
